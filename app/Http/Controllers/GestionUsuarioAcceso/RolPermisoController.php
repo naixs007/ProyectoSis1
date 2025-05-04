09a91bb9e54c770a84cdd92a317bce74e4366956
Bryan <b.a.h.007@live.com>
2025-05-04 00:25:35 -0400

    cargando proyecto

---
+++ app/Http/Controllers/GestionUsuarioAcceso/RolPermisoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\GestionUsuarioAcceso;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RolPermisoController extends Controller
+{
+    //
+}
---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ asset('img/logo_manpower.png') }}" alt="Manpower" class="h-10">
+
